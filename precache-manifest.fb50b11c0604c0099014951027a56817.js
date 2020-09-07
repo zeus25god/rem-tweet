801bb468ff0ded1d80b5862fb9b211c66c56162c
zeus25god <preetisahoo251299@gmail.com>
2020-09-07 11:13:51 +0530

    Updates

---
+++ precache-manifest.fb50b11c0604c0099014951027a56817.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57a03d4582d6e372c94d34225ffcac3b",
+    "url": "/rem-tweet/index.html"
+  },
+  {
+    "revision": "d69f827966a619c733a3",
+    "url": "/rem-tweet/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2c262d41e39d1b8e8931",
+    "url": "/rem-tweet/static/js/2.2e6cef01.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rem-tweet/static/js/2.2e6cef01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69f827966a619c733a3",
+    "url": "/rem-tweet/static/js/main.8420ad2b.chunk.js"
+  },
+  {
+    "revision": "9cd4c4dd13c6b83b778d",
+    "url": "/rem-tweet/static/js/runtime-main.88011822.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rem-tweet/static/media/logo.5d5d9eef.svg"
+  }
+]);
