895782c35aa58d9214a0f425bd3b7411a88d4a65
zeus25god <preetisahoo251299@gmail.com>
2020-09-07 11:15:34 +0530

    do

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         alt = "logo" / >
         <
         p >
-        Edit it bro < code > src / App.js < /code> and save to reload. < /
+        Edit bro < code > src / App.js < /code> and save to reload. < /
         p > <
         a className = "App-link"
         href = "https://reactjs.org"
