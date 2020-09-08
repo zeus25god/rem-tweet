e13b9cee9475ffc2b3a4bd2fd0a1b19544cf1dec
zeus25god <preetisahoo251299@gmail.com>
2020-09-08 07:27:31 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
-import React from 'react';
-import Buttontwi from './Components/Buttontwi';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import 'antd/dist/antd.css';
+import { Typography } from 'antd';
+import { Button } from 'antd';
+
+import 'antd/dist/antd.css';
+import { Row, Col, Divider } from 'antd';
+
+const { Paragraph } = Typography;
+const style = { background: '#34416e', padding: '8px 0', maxWidth: '1200px' };
+
+function Buttontwi() {
+
+    const [lengthLimitedStr, setLengthLimitedStr] = useState(
+        'This is an editable text with limited length.',
+    );
     return ( <
-        div style = {
-            { height: '620px', width: '1280px', background: '#252C48' }
+        div >
+        <
+        Divider orientation = "center" >
+        <
+        h2 style = {
+            { color: 'whitesmoke' }
+        } > Dissapearing Tweets < /h2>
+
+        <
+        /Divider> <
+        Row >
+        <
+        Col className = "gutter-row"
+        span = { 18 }
+        style = {
+            { maxHeight: '200px', marginLeft: '160px' }
         } >
         <
-        div className = "App" >
+        div style = { style } >
         <
-        Buttontwi / >
-        <
-        /div> < /
+        Paragraph editable = {
+            {
+                onChange: setLengthLimitedStr,
+                autoSize: { maxRows: 5, minRows: 3 }
+            }
+        }
+        style = {
+            { background: 'white', marginTop: '110px', marginLeft: '30px', maxWidth: '900px' }
+        } > { lengthLimitedStr } <
+        /Paragraph> <
+        Button type = "primary" >
+        Tweet <
+        /Button> < /
         div >
-    );
+
+        <
+        /Col> < /
+        Row > <
+        Row style = {
+            { background: "#34416e", marginLeft: '160px', marginTop: '100px', maxWidth: '960px' }
+        } >
+        <
+        Col className = "gutter-row"
+        span = { 18 } >
+        <
+        div style = { style } > < /div> < /
+        Col > <
+        /Row>
+
+
+
+        <
+        /div>
+    )
 }
 
-export default App;
+export default Buttontwi
--- src/Components/Buttontwi.js
+++ src/Components/Buttontwi.js
@@ -8,7 +8,7 @@ import 'antd/dist/antd.css';
 import { Row, Col, Divider } from 'antd';
 
 const { Paragraph } = Typography;
-const style = { background: '#252C48', padding: '8px 0' };
+const style = { background: '#34416e', padding: '8px 0', maxWidth: '1200px' };
 
 function Buttontwi() {
 
@@ -18,13 +18,20 @@ function Buttontwi() {
     return ( <
         div >
         <
-        Divider orientation = "center" > Responsive < /Divider> <
+        Divider orientation = "center" >
+        <
+        h2 style = {
+            { color: 'whitesmoke' }
+        } > Dissapearing Tweets < /h2>
+
+        <
+        /Divider> <
         Row >
         <
         Col className = "gutter-row"
         span = { 18 }
         style = {
-            { background: "#242b42" }
+            { maxHeight: '200px', marginLeft: '160px' }
         } >
         <
         div style = { style } >
@@ -32,19 +39,24 @@ function Buttontwi() {
         Paragraph editable = {
             {
                 onChange: setLengthLimitedStr,
-                autoSize: { maxRows: 5, minRows: 3 },
+                autoSize: { maxRows: 5, minRows: 3 }
             }
         }
         style = {
-            { background: 'white', marginTop: '110px', marginLeft: '270px' }
+            { background: 'white', marginTop: '110px', marginLeft: '30px', maxWidth: '900px' }
         } > { lengthLimitedStr } <
-        /Paragraph>
+        /Paragraph> <
+        Button type = "primary" >
+        Tweet <
+        /Button> < /
+        div >
 
         <
-        /div> < /
-        Col > <
-        /Row> <
-        Row >
+        /Col> < /
+        Row > <
+        Row style = {
+            { background: "#34416e", marginLeft: '160px', marginTop: '100px', maxWidth: '960px' }
+        } >
         <
         Col className = "gutter-row"
         span = { 18 } >
@@ -53,10 +65,7 @@ function Buttontwi() {
         Col > <
         /Row>
 
-        <
-        Button type = "primary" >
-        Tweet <
-        /Button>
+
 
         <
         /div>
