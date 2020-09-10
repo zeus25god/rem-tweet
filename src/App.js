7df147efd07f30329ca63d864e448ec456b6d113
zeus25god <preetisahoo251299@gmail.com>
2020-09-10 07:49:37 +0530

    gh

--- src/App.js
+++ src/App.js
@@ -1,106 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Buttontwi from './Components/Buttontwi';
+import './App.css';
+import { Provider } from 'react-redux'
+import store from './Redux/store'
 
-import 'antd/dist/antd.css';
-import { Typography } from 'antd';
-import { Button } from 'antd';
-import { DatePicker, Space } from 'antd';
-
-import 'antd/dist/antd.css';
-import { Row, Col, Divider } from 'antd';
-
-import { useSelector, useDispatch } from 'react-redux'
-
-import { doTweet, doMsg } from '../Redux/action';
-
-
-
-const { Paragraph } = Typography;
-const style = { background: '#34416e', padding: '8px 0' };
-
-
-function onChange(date, dateString) {
-    console.log(date, dateString);
-}
-
-
-
-function Buttontwi() {
-
-
-    const notw = useSelector(state => state.notw)
-    const msgw = useSelector(state => state.msgw)
-    const dispatch = useDispatch()
-
-    const [txt, setTxt] = useState({ ntxt: '' })
+function App() {
     return ( <
-        div >
-
+        Provider store = { store } >
         <
-        Divider orientation = "center" >
-        <
-        h2 style = {
-            { color: 'whitesmoke' }
-        } > Dissapearing Tweets < /h2> < /
-        Divider > <
-        Row >
-        <
-        Col className = "gutter-row"
-        span = { 18 }
-        style = {
-            { maxHeight: '200px', marginLeft: '160px' }
+        div style = {
+            { height: '620px', width: '1280px', background: '#252C48' }
         } >
         <
-        div style = { style } >
+        div className = "App" >
         <
-        textarea className = 'txt'
-        style = {
-            { background: 'white', marginTop: '50px', marginLeft: '30px', width: '700px', maxHeight: '50px' }
-        }
-        onChange = { dispatch(doMsg()) } >
-
+        Buttontwi / >
         <
-        /textarea> < /
+        /div> < /
         div > <
-        div style = { style } >
-        <
-        DatePicker onChange = { onChange }
-        />
-
-        <
-        h3 style = {
-            { color: 'white' }
-        } > No of Tweets - { notw } < /h3> <
-        Button type = "submit"
-        style = {
-            { background: "blue", color: "whitesmoke" }
-        }
-        onClick = {
-            () => dispatch(doTweet())
-        } >
-        Tweet <
-        /Button> < /
-        div >
-
-        <
-        /Col> < /
-        Row > <
-        Row >
-        <
-        Col className = "gutter-row"
-        span = { 18 }
-        style = {
-            { margin: '60px 0px 0px 160px' }
-        } >
-        <
-        div style = { style } > < /div> < /
-        Col > <
-        /Row>
-
-
-
-        <
-        /div>
-    )
+        /Provider>
+    );
 }
 
-export default Buttontwi
+export default App;
